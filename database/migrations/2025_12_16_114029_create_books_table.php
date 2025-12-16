cdccf09da5ad5731e855ab0a325a1dc527b805c5
zakari2-ben <riariazakaria6@gmail.com>
2025-12-16 16:27:43 +0100

    Add Book model and migration

---
+++ database/migrations/2025_12_16_114029_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('designation')->unique()->required();
+            $table->text('description');
+            $table->string('type')->default('Texte');
+            $table->string('langue')->default('Francais');
+            $table->string('editeur')->default('Anonyme');
+            $table->string('categorie')->default('Nouveau');
+            $table->double('prix')->default(0);
+            $table->string('auteur')->default('Anonyme');
+            $table->string('cover')->default('no_cover.jpg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
